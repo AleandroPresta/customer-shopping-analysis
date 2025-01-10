2c9ae950756a3496e6ab386a964a31f425d6bbaa
Aleandro Presta <37838171+AleandroPresta@users.noreply.github.com>
2025-01-10 12:39:16 +0100

    Moved all plotting functions to utils.py

--- utils.py
+++ utils.py
@@ -12,3 +12,30 @@ def plot_item_distribution(dataset: pd.DataFrame, column_name: str):
     plt.xlabel('Number of Purchases')
     plt.ylabel(column_name)
     plt.show()
+
+
+def plot_numerical_distribution(dataset: pd.DataFrame, column_name: str):
+    plt.figure(figsize=(12, 8))
+    sns.histplot(dataset[column_name], kde=True, color='skyblue', bins=30)
+    plt.xlabel(column_name, fontsize=14)
+    plt.ylabel('Number of purchases', fontsize=14)
+    plt.title(f'Distribution of {column_name}', fontsize=16)
+    plt.grid(True, linestyle='--', alpha=0.7)
+    plt.xticks(fontsize=12)
+    plt.yticks(fontsize=12)
+    plt.show()
+
+
+def plot_pie_chart(dataset: pd.DataFrame, column_name: str):
+    counting = dataset[column_name].value_counts()
+    counting.plot(kind='pie', autopct='%1.1f%%', startangle=90, legend=True)
+    plt.ylabel('')
+    plt.title(f'{column_name} Distribution')
+    plt.show()
+    
+    
+def plot_correlation_matrix(dataset: pd.DataFrame):
+    plt.figure(figsize=(14, 10))
+    sns.heatmap(dataset.corr(), annot=True, cmap='coolwarm', linewidths=.5)
+    plt.title('Correlation Matrix')
+    plt.show()
